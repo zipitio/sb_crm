849bc7f46322e14f97d0e2be5d657da164707c52
Zipitio <SGRiveraC@gmail.com>
2023-01-26 16:57:44 -0500

    add basic validations to Client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,6 @@
 class Client < ApplicationRecord
 
+	validates :first_name, presence :true, length: {minimum: 3, maximum: 100}
+	validates :last_name, presence :true, length: {minimum: 3, maximum: 100}
+	validates :company, presence :true, length: {minimum: 1, maximum: 100}
 end
