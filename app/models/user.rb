08be0384c21250d7927fe8d71218c15535ae03fa
zipitio <62092328+zipitio@users.noreply.github.com>
2023-02-09 11:58:25 -0500

    downcase user emails before saving and add User to client show

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	before_save { self.email = email.downcase }
     has_many :clients
     validates :username, presence: true, 
         uniqueness: { case_sensitive: false}, 
