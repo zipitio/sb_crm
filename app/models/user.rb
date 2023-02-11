511f652616c5493a5c480266b76a8324b735bcf3
zipitio <62092328+zipitio@users.noreply.github.com>
2023-02-11 11:44:47 -0500

    update signup page and get devise controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-	before_save { self.email = email.downcase }
-    has_many :clients
-    validates :username, presence: true, 
-        uniqueness: { case_sensitive: false}, 
-        length: {minimum: 3, maximum: 25}
-    validates :email, presence: true, 
-        uniqueness: { case_sensitive: false}, 
-        length: { maximum: 105},
-        format: { with: URI::MailTo::EMAIL_REGEXP}
+  has_many :clients
 end
