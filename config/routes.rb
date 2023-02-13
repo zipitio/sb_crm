a0f644212bea62e7a2b5ea241f168e1690249724
zipitio <62092328+zipitio@users.noreply.github.com>
2023-02-13 11:43:23 -0500

    redirect user to clients page after sign up or login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users 
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root 'pages#index'
   get '/stats', to: 'pages#stats'
-
+  get '/user' => "clients#index", :as => :user_root
   resources :clients
+
 end
