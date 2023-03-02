ece184f6c7c263e34908878ef8f4df46dc515807
zipitio <62092328+zipitio@users.noreply.github.com>
2023-03-02 16:49:11 -0500

    fix bugs with new quote form

--- app/javascript/controllers/form_element_controller.js
+++ app/javascript/controllers/form_element_controller.js
@@ -4,7 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["submitbtn"]
   connect() {
-    this.submitbtnTarget.hidden = false
+    this.submitbtnTarget.hidden = true
   }
 
   remotesubmit() {
